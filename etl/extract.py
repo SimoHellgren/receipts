f0765fe1c5eabef467b544481f21cfd03626c931
SimoHellgren <hellgrensimo@gmail.com>
2021-09-14 20:49:11 +0300

    generalized date range extraction

--- etl/extract.py
+++ etl/extract.py
@@ -11,9 +11,8 @@ from dotenv import load_dotenv
 drop = lambda n, it: islice(it, n, None)
 tail = partial(drop, 1)
 
-
-
-def extract_by_date(folder: str, dt: datetime):
+def extract_datetime_range(folder: str, start_dt: datetime, end_dt: datetime):
+    '''Extract data from a S3 bucket / logical folder if modified time is in [start_dt, end_dt)'''
     # secrets from environment
     load_dotenv()
     
@@ -26,7 +25,13 @@ def extract_by_date(folder: str, dt: datetime):
     data = tail(bucket.objects.filter(Prefix=folder))
 
     # filter to relevant date
-    predicate = lambda x: dt <= x.last_modified < (dt + timedelta(days=1))
+    predicate = lambda x: start_dt <= x.last_modified < end_dt
     modified_since = filter(predicate, data)
 
     return modified_since
+
+
+def extract_by_date(folder: str, start_dt: datetime):
+    '''special case of `extract_datetime_range` where end_dt is one day from start_dt'''
+    end_dt = start_dt + timedelta(days=1)
+    return extract_datetime_range(folder, start_dt, end_dt)
