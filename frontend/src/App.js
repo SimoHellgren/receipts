a08593370bdce5539fa9db56eb6eee7c8bdefa7d
SimoHellgren <hellgrensimo@gmail.com>
2021-09-17 21:06:52 +0300

    minimal receipt view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const Receipt = () => {
-  return <h2>hello</h2>
+const APIRUL = 'http://localhost:8000/'
+
+const get_one_receipt = () => {
+  const request = axios.get(APIRUL + "receipts")
+  return request.then(r => r.data[0])
+}
+
+const Receipt = ({receipt}) => {
+  
+  return (<div style={{whiteSpace: "break-spaces", fontFamily: "monospace"}}>
+    <h2>Result: {receipt.id}</h2>
+    <p>{receipt.reprint}</p>
+  </div>
+  )
 }
 
 
 function App() {
-  return <Receipt/>
+  const [receipt, setReceipt] = useState({})
+
+  useEffect( () => {
+    get_one_receipt().then(r => setReceipt(r))
+  }, [])
+
+  console.log(receipt.reprint)
+
+  return <Receipt receipt={receipt}/>
 }
 
 export default App;
