74a9d1f7d5ce9bc317a75f47ed1316c4b61db08d
SimoHellgren <hellgrensimo@gmail.com>
2021-09-22 18:00:37 +0300

    minimal post products

--- etl/routers/products.py
+++ etl/routers/products.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from ..dependencies import get_db
 from .. import models
+from .. import schemas
 
 router = APIRouter(
     prefix='/products',
@@ -12,3 +13,17 @@ router = APIRouter(
 @router.get('/')
 def get_products(db: Session = Depends(get_db)):
     return db.query(models.Product).all()
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    db_product=models.Product(
+        id=product.id,
+        name=None
+    )
+
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+
+    return db_product
