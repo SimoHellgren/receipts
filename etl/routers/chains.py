394a969d9b43da40361525803034cfb9954f0678
SimoHellgren <hellgrensimo@gmail.com>
2021-09-23 17:32:53 +0300

    initial post chains

--- etl/routers/chains.py
+++ etl/routers/chains.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from ..dependencies import get_db
 from .. import models
+from .. import schemas
 
 router = APIRouter(
     prefix='/chains',
@@ -12,3 +13,16 @@ router = APIRouter(
 @router.get('/')
 def get_chains(db: Session = Depends(get_db)):
     return db.query(models.Chain).all()
+
+@router.post('/')
+def create_chain(chain: schemas.Chain, db: Session = Depends(get_db)):
+    db_chain = models.Chain(
+        id=chain.id,
+        name=chain.name
+    )
+
+    db.add(db_chain)
+    db.commit()
+    db.refresh(db_chain)
+
+    return db_chain
