15a431fddbc99efc39d129afab43cf195167e81f
SimoHellgren <hellgrensimo@gmail.com>
2021-09-23 17:47:31 +0300

    initial post store

--- etl/routers/stores.py
+++ etl/routers/stores.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from ..dependencies import get_db
 from .. import models
+from .. import schemas
 
 router = APIRouter(
     prefix='/stores',
@@ -12,3 +13,21 @@ router = APIRouter(
 @router.get('/')
 def get_stores(db: Session = Depends(get_db)):
     return db.query(models.Store).all()
+
+@router.post('/')
+def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db)):
+    # generate a store id from chain id and store name. Use also as store_name,
+    # for the sake of consistency, at least for now
+    store_id = store_name = f'{store.chain_id} {store.name}'
+
+    db_store = models.Store(
+        id=store_id,
+        name=store_name,
+        chain_id=store.chain_id
+    )
+
+    db.add(db_store)
+    db.commit()
+    db.refresh(db_store)
+
+    return db_store
--- etl/schemas.py
+++ etl/schemas.py
@@ -57,7 +57,22 @@ class Chain(BaseModel):
     '''Not implementing separate model for creation, as ID needs to always be provided,
         at least as long as the DB relies on system keys as PKs. If and when this changes
         to generating uniform keys, this model should change as well.
-
     '''
     id: str
     name: str
+
+
+class StoreBase(BaseModel):
+    name: str
+    chain_id: str
+
+
+class Store(StoreBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class StoreCreate(StoreBase):
+    pass
