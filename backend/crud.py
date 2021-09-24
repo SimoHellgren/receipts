0bd4704ec8faf1536cf9c3e7b6e0098f3e916ee5
SimoHellgren <hellgrensimo@gmail.com>
2021-09-24 17:34:31 +0300

    extracted crud from stores

--- backend/crud.py
+++ backend/crud.py
@@ -17,3 +17,21 @@ def create_chain(db: Session, chain: schemas.Chain):
     db.refresh(db_chain)
 
     return db_chain
+
+
+def get_stores(db: Session):
+    return db.query(models.Store).all()
+
+
+def create_store(db: Session, store: schemas.StoreCreate):
+    db_store = models.Store(
+        id=store.id,
+        name=store.name,
+        chain_id=store.chain_id
+    )
+
+    db.add(db_store)
+    db.commit()
+    db.refresh(db_store)
+
+    return db_store
--- backend/routers/stores.py
+++ backend/routers/stores.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ..dependencies import get_db
-from .. import models
+from .. import crud
 from .. import schemas
 
 
@@ -13,19 +13,9 @@ router = APIRouter(
 
 @router.get('/')
 def get_stores(db: Session = Depends(get_db)):
-    return db.query(models.Store).all()
+    return crud.get_stores(db)
 
 
 @router.post('/')
 def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db)):
-    db_store = models.Store(
-        id=store.id,
-        name=store.name,
-        chain_id=store.chain_id
-    )
-
-    db.add(db_store)
-    db.commit()
-    db.refresh(db_store)
-
-    return db_store
+    return crud.create_store(db, store)
