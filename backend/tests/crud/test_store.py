85a2fdcb6c3f89d4d6aaea6cd3a145a4c834babe
SimoHellgren <hellgrensimo@gmail.com>
2021-09-26 14:11:18 +0300

    test for failing store creation

--- backend/tests/crud/test_store.py
+++ backend/tests/crud/test_store.py
@@ -1,3 +1,6 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+
 from backend import crud
 
 
@@ -15,3 +18,10 @@ def test_create_store(chain_test_data, store_test_data, test_db_session):
     assert db_store.name == store_in.name
     assert db_store.chain_id == store_in.chain_id
 
+
+def test_create_store_fk_fail(store_test_data, test_db_session):
+    '''Store with a non-existing chain_id shan't be created'''
+    store_in = store_test_data
+    
+    with pytest.raises(IntegrityError):
+        db_store = crud.create_store(test_db_session, store_in)
