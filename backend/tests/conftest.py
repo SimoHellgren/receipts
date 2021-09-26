cc34d591d969331816c1d931fcaa2b2befc8241e
SimoHellgren <hellgrensimo@gmail.com>
2021-09-26 13:42:33 +0300

    made test data

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 import pytest
@@ -10,6 +11,7 @@ from fastapi.testclient import TestClient
 from backend.api import app
 from backend.models import Base
 from backend.dependencies import get_db
+from backend import schemas
 
 load_dotenv()
 
@@ -67,3 +69,36 @@ def test_db_session():
 def client():
     with TestClient(app) as c:
         yield c
+
+
+@pytest.fixture
+def chain_test_data():
+    chain1 = schemas.Chain(id='CHAIN_1', name='Chain 1')
+    return chain1
+
+
+@pytest.fixture
+def store_test_data():
+    store1 = schemas.StoreCreate(id='STORE_1', name='Store 1', chain_id='CHAIN_1')
+    return store1
+
+
+@pytest.fixture
+def paymentmethod_test_data():
+    paymentmethod1 = schemas.Paymentmethod(id='CASH', payer=None)
+    return paymentmethod1
+
+
+@pytest.fixture
+def receipt_test_data():
+    receipt1 = schemas.ReceiptCreate(
+        datetime=datetime(2021, 1, 1, 0, 0, 0, 0),
+        store_id='STORE_1',
+        paymentmethod_id='CASH',
+        total=11.11,
+        id='test_id',
+        reprint='Test reprint',
+        etag='iuyweriuyweriuyhsdkjhskjfh'
+    )
+
+    return receipt1
