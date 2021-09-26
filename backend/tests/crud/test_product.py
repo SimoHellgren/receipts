7a499050af41f534ff58d7330c54b0a5e9061d68
SimoHellgren <hellgrensimo@gmail.com>
2021-09-26 16:28:30 +0300

    added test for get product

--- backend/tests/crud/test_product.py
+++ backend/tests/crud/test_product.py
@@ -1,4 +1,12 @@
-from backend import crud
+from backend import crud, schemas
+
+def test_create_product(test_db_session):
+    product_in = schemas.ProductCreate(id='PRODUCT_2')
+
+    db_product = crud.create_product(test_db_session, product_in)
+
+    assert db_product.id == product_in.id
+
 
 def test_get_product(test_product, test_db_session):
     get_product = crud.get_product(test_db_session, test_product.id)
