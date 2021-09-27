6e66e5999ce6ac548537eebd999a56ba15bac4db
SimoHellgren <hellgrensimo@gmail.com>
2021-09-27 17:59:22 +0300

    endpoint and test for PUT /store

--- backend/routers/stores.py
+++ backend/routers/stores.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from ..dependencies import get_db
 from .. import crud
@@ -24,3 +26,21 @@ def get_store(store_id: str, db = Depends(get_db)):
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db)):
     return crud.create_store(db, store)
+
+
+@router.put('/{store_id}')
+def update_store(store_id: str, store: schemas.Store, db: Session = Depends(get_db)):
+    db_store = crud.get_store(db, store_id)
+
+    if db_store:
+        response_code = status.HTTP_200_OK
+        new_store = crud.update_store(db, store)
+
+    else:
+        response_code = status.HTTP_201_CREATED
+        new_store = crud.create_store(db, store)
+
+    return JSONResponse(
+        status_code=response_code,
+        content=jsonable_encoder(new_store)
+    )
--- backend/tests/api/test_stores.py
+++ backend/tests/api/test_stores.py
@@ -29,3 +29,28 @@ def test_post(client):
     data = response.json()
 
     assert data == store_in
+
+
+def test_put_new(client):
+    store = {'id': 'NEWSTORE', 'name': 'A new store', 'chain_id': 'CHAIN_1'}
+
+    response = client.put(f"/stores/{store['id']}", json=store)
+
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data == store
+
+
+def test_put_existing(client, test_data):
+    test_store = test_data['store']
+    store = {'id': test_store.id, 'name': 'A new store', 'chain_id': 'CHAIN_1'}
+
+    response = client.put(f"/stores/{store['id']}", json=store)
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data == store
