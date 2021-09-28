6d07e8519f88e5be0b1cf280258e27d5e6cdafb8
SimoHellgren <hellgrensimo@gmail.com>
2021-09-28 17:24:04 +0300

    route and tests for PUT /paymentmethod

--- backend/routers/paymentmethods.py
+++ backend/routers/paymentmethods.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from ..dependencies import get_db
 from .. import crud
@@ -19,3 +21,18 @@ def get_paymentmethods(db: Session = Depends(get_db)):
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create_paymentmethod(paymentmethod: schemas.Paymentmethod, db: Session = Depends(get_db)):
     return crud.paymentmethod.create(db, obj_in=paymentmethod)
+
+
+@router.put('/{paymentmethod_id}')
+def update_paymentmethod(paymentmethod_id: str, paymentmethod: schemas.Paymentmethod, db: Session = Depends(get_db)):
+    db_obj = crud.paymentmethod.get(db, paymentmethod_id)
+
+    if db_obj:
+        response_code = status.HTTP_200_OK
+        new_obj = crud.paymentmethod.update(db, db_obj=db_obj, obj_in=paymentmethod)
+
+    else:
+        response_code = status.HTTP_201_CREATED
+        new_obj = crud.paymentmethod.create(db, obj_in=paymentmethod)
+
+    return JSONResponse(status_code=response_code, content=jsonable_encoder(new_obj))
--- backend/tests/api/test_paymentmethods.py
+++ backend/tests/api/test_paymentmethods.py
@@ -6,7 +6,6 @@ def test_get(client):
     assert len(response.json()) > 0
 
 
-
 def test_post(client):
     pm = {'id': 'PayPal', 'payer': None}
 
@@ -17,3 +16,27 @@ def test_post(client):
     data = response.json()
 
     assert data == pm 
+
+
+def test_put_new(client):
+    obj_in = {'id': 'PayPal', 'payer': 'E. Musk'}
+
+    response = client.put(f"/paymentmethods/{obj_in['id']}", json=obj_in)
+
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data == obj_in
+
+
+def test_put_existing(client, test_data):
+    obj_in = {'id': test_data['paymentmethod'].id, 'payer': 'E. Musk'}
+
+    response = client.put(f"/paymentmethods/{obj_in['id']}", json=obj_in)
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data == obj_in
