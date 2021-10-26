d89c9411c65edae10c1d9e837de6960c02787572
SimoHellgren <hellgrensimo@gmail.com>
2021-10-26 20:20:38 +0300

    receipt model and schema now have integer total instead of numeric/float

--- backend/app/schemas/receipt.py
+++ backend/app/schemas/receipt.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, StrictInt
 
 class ReceiptBase(BaseModel):
     datetime: datetime
     store_id: str
     paymentmethod_id: str
-    total: float
+    total: StrictInt
     id: str
     reprint: str
     etag: str
